8c4f74fcf8b1eb80acfe180474df3de160716124
Kishan Patel <16578929+ksp37@users.noreply.github.com>
2021-04-14 16:56:35 +0100

    Added classes for Category and Slug.

---
+++ view_builder/model/model.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import ForeignKey, Column, Integer, String
+
+Base = declarative_base()
+
+class Slug(Base):
+    __tablename__ = 'slug'
+    id = Column(Integer, primary_key=True)
+    prefix = Column(String)
+    slug = Column(String)
+
+    category = relationship("Category", back_populates="slug")
+
+    def __repr__(self):
+        return f"Slug(id={self.id!r}, prefix={self.prefix!r}, slug={self.slug!r})"
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    slug_id = Column(Integer, ForeignKey('slug.id'))
+    category = Column(String)
+    reference = Column(String)
+    name = Column(String)
+
+    slug = relationship("Slug", back_populates="category")
+
+    def __repr__(self):
+        return f"Category(id={self.id!r}, name={self.name!r}, category={self.category!r}, " \
+               f"reference={self.reference!r}, slug={self.slug!r})"
