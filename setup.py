d61ced1e391692bd044de9e72c69a07d4a14d8b2
Jon Bramley <brammers@gmail.com>
2021-06-17 16:28:33 +0100

    fix: include model in package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="view_builder",
     version="0.1",
-    packages=["view_builder"],
+    packages=find_packages(),
     install_requires=[
         "Click",
     ],
